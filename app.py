e0cae4def7915c066f83d1c3610059a6c56c795f
Saniamo <mercysaniamo@gmail.com>
2021-09-02 18:18:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('/Users/saniamo/Desktop/Breast_Cancer_copy/final_model.sav', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = ['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
+       'compactness_mean', 'concavity_mean', 'perimeter_se', 'area_se',
+       'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst',
+       'compactness_worst', 'concavity_worst']
+    
+    df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
+        
+    if output == 0:
+        res_val = "** Breast Cancer Predicted**"
+    else:
+        res_val = " No Breast Cancer"
+        
+
+    return render_template('home.html', prediction_text='ML algorithm predicts{}'.format(res_val))
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
