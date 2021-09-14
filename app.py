683c76d61b91d178211919f46ece49c52847e69c
Saniamo <mercysaniamo@gmail.com>
2021-09-14 15:31:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,20 @@
-
+from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
-import pandas as pd
-from flask import Flask, request, render_template
 import pickle
+import json
 
 app = Flask(__name__)
-model = pickle.load(open('./final_model.sav', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    input_features = [float(x) for x in request.form.values()]
-    features_value = [np.array(input_features)]
-    
-    features_name = ['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
-       'compactness_mean', 'concavity_mean', 'perimeter_se', 'area_se',
-       'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst',
-       'compactness_worst', 'concavity_worst']
-    
-    df = pd.DataFrame(features_value, columns=features_name)
-    output = model.predict(df)
-        
-    if output == 0:
-        res_val = "** Breast Cancer Predicted**"
-    else:
-        res_val = " No Breast Cancer"
+@app.route('/api/',methods=['POST'])
+def predict():    
+    data = request.get_json()
+    prediction = np.array2string(model.predict(data))
+
+    return jsonify(prediction)
         
 
-    return render_template('home.html', prediction_text='ML algorithm predicts{}'.format(res_val))
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    model = pickle.load(open('./final_model.sav', 'rb'))
+    app.run(debug=True, host='0.0.0.0')
