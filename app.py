eaa99537443b37d98a80dfd989125d46ee8f4ed3
Saniamo <mercysaniamo@gmail.com>
2021-09-14 16:02:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def predict():
 
 if __name__ == '__main__':
     model = pickle.load(open('./final_model.sav', 'rb'))
-    app.run(debug=True, host='ace-breastcancer-prediction.herokuapp.com')
+    app.run(debug=True)
